1d1f2afcb746241d89ab945b3065989b7d7db630
Vidhya ES <158551716+VidhyaES@users.noreply.github.com>
2026-03-13 00:35:08 +0530

    Change app.run to specify host and port

--- app.py
+++ app.py
@@ -50,4 +50,4 @@ def upload():
     return render_template('analyze.html', error="Invalid file type")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
